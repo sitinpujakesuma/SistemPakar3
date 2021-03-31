cc63369e3ecaf86f29c629ff1165c9c2acbf0ced
sitinpujakesuma <sitinpujakesuma@gmail.com>
2021-03-31 13:55:13 +0700

    Upload Tugas Sistem Pakar 3

---
+++ Tugas3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 11:09:51 2021
+
+@author: Siti N Puja Kesuma
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+datalatih = pd.read_excel("data testing.xlsx")
+datalatih.head(11)
